f29832f18be6e734d742078a8427f1ad5c56e008
lsphillips <lsphillips.mail@gmail.com>
2017-05-14 21:10:50 +0100

    Added TypeScript definitions.

---
+++ Crumble.d.ts
@@ -0,0 +1,34 @@
+// Type definitions for Crumble
+// --------------------------------------------------------
+
+export interface Crumbs
+{
+    name            : string;
+    value?          : string;
+    age?            : number;
+    expires?        : string | number | Date;
+    path?           : string;
+    domain?         : string;
+    secure?         : boolean;
+    firstPartyOnly? : boolean;
+};
+
+// --------------------------------------------------------
+
+export function isCookiesEnabled() : boolean;
+
+// --------------------------------------------------------
+
+export function hasCookie(name : string) : boolean;
+
+// --------------------------------------------------------
+
+export function getCookie(name : string) : string;
+
+// --------------------------------------------------------
+
+export function setCookie(crumbs : Crumbs, cookieValue? : string) : void;
+
+// --------------------------------------------------------
+
+export function removeCookie(crumbs : Crumbs) : void;
