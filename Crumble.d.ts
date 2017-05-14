01a0ad94e001089962df5b04079d6eaccb014387
lsphillips <lsphillips.mail@gmail.com>
2017-05-14 21:34:09 +0100

    Fixed a syntax error in the TypeScript type definitions.

--- Crumble.d.ts
+++ Crumble.d.ts
@@ -11,7 +11,7 @@ export interface Crumbs
     domain?         : string;
     secure?         : boolean;
     firstPartyOnly? : boolean;
-};
+}
 
 // --------------------------------------------------------
 
